a6176a1ba300ce80ffed387fa1be56d43bbee71c
italosanto <italosanto@INOTE706>
2020-02-17 17:09:25 -0300

    API Versioning with Swagger

---
+++ src/main/java/com/calendar/fiserv/calendar/config/SwaggerBootstrap.java
@@ -0,0 +1,23 @@
+package com.calendar.fiserv.calendar.config;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class SwaggerBootstrap extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+		
+		
+		
+	}
+
+}
--- src/main/java/com/calendar/fiserv/calendar/config/SwaggerConfig.java
+++ src/main/java/com/calendar/fiserv/calendar/config/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package com.calendar.fiserv.calendar.config;
 
-import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicate;
+
 import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,15 +19,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.select()
-			.apis(controllers())
-			.paths(PathSelectors.any())
-			.build();
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(controllers()).paths(PathSelectors.any()).build()
+				.apiInfo(getApiInfo());
+	}
+
+	private ApiInfo getApiInfo() {
+		return new ApiInfoBuilder().title("Foo Bar").description("API Calendar").version("1.1.0").build();
 	}
 
 	private static Predicate<RequestHandler> controllers() {
-		return RequestHandlerSelectors
-			.basePackage("com.calendar.fiserv.calendar");
+		return RequestHandlerSelectors.basePackage("com.calendar.fiserv.calendar");
 	}
 }
